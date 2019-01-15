0dd59ae17c37e4b9c1656e5a7775beb752c501bd
alikatkar <ali.katkar@aurea.com>
2019-01-15 20:04:01 +0200

    lesson1 solutions added

---
+++ src/main/java/com/akatkar/functional/exercises/chapter1/solutions/Exercise01.java
@@ -0,0 +1,33 @@
+package com.akatkar.functional.exercises.chapter1.solutions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * All exercises should be completed using Lambda expressions and the new
+ * methods added to JDK 8 where appropriate. There is no need to use an explicit
+ * loop in any of the code. Use method references rather than full lambda
+ * expressions wherever possible.
+ */
+public class Exercise01 {
+
+    /**
+     * Exercise 01
+     *
+     * Create a string that consists of the first letter of each word in the
+     * list of Strings provided.
+     */
+    private void exercise1() {
+        List<String> list = Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+
+        StringBuilder sb = new StringBuilder();
+        list.forEach(s -> sb.append(s.charAt(0)));
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        new Exercise01()
+                .exercise1();
+    }
+}
