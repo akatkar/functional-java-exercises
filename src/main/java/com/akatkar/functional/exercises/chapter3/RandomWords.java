eeaea7a20ceb3ddec471240469ca1c890e0cbffd
alikatkar <ali.katkar@aurea.com>
2019-01-15 20:09:36 +0200

    lesson3 exercises added

---
+++ src/main/java/com/akatkar/functional/exercises/chapter3/RandomWords.java
@@ -0,0 +1,55 @@
+package com.akatkar.functional.exercises.chapter3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Class to generate a list of random words
+ *
+ * @author Simon Ritter (@speakjava)
+ */
+public class RandomWords {
+
+    private final List<String> sourceWords;
+
+    /**
+     * Constructor
+     *
+     * @throws IOException If the source words file cannot be read
+     */
+    public RandomWords() throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("target/classes/words.txt"))) {
+            sourceWords = null;    // YOUR CODE HERE
+
+            System.out.println("Loaded " + sourceWords.size() + " words");
+        }
+    }
+
+    /**
+     * Create a list of a given size containing random words
+     *
+     * @param listSize The size of the list to create
+     * @return The created list
+     */
+    public List<String> createList(int listSize) {
+        Random rand = new Random();
+        List<String> wordList = null; // YOUR CODE HERE
+
+        return wordList;
+    }
+
+    /**
+     * Return the list of all source words, which cannot be modified
+     *
+     * @return The unmodifiable list of all source words
+     */
+    public List<String> allWords() {
+        return Collections.unmodifiableList(sourceWords);
+    }
+}
